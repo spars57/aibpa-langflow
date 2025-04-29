144cbd81e30756d0edb7d8eb5db3cae242c45aae
bruno.moisao <bruno.moisao@1global.com>
2025-04-29 23:09:08 +0100

    Add integration with GPT4All

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
-import { LangflowClient } from "@datastax/langflow-client";
-
-const apiKey = process.env.LANGFLOW_API_KEY;
-const langflowId = process.env.LANGFLOW_ID;
-const flowId = process.env.FLOW_ID;
-
-const client = new LangflowClient({
-  langflowId,
-  apiKey,
-});
-
-const flow = client.flow(flowId);
-
 import express from "express";
 const app = express();
 const port = process.env.PORT || 8081;
 
+const url = "http://localhost:4891/v1/chat/completions";
+
 app.use(express.json());
 
 app.route("/ask").post(async (req, res) => {
+  console.log(req);
   const question = req?.body?.question;
+  console.log("Sending question...", question);
   if (!question) {
     res.status(400).send("No question provided");
     return;
   }
-  const result = await flow.run(question, {}).catch((err) => {
-    console.log(err);
+  const data = {
+    model: "Phi-3 Mini Instruct",
+    messages: [{ role: "user", content: question }],
+    max_tokens: 1000,
+    temperature: 0.5,
+  };
+  const response = await fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  }).catch((err) => {
     res.status(500).send(err);
   });
-  const chatOutputText = result?.chatOutputText();
-  const response = {
-    question: question,
-    response: chatOutputText,
-  };
-  res.status(200).send(response);
+
+  const json = await response.json();
+  console.log(JSON.stringify(json));
+
+  const message = json.choices[0].message.content.split("</think>")?.[1];
+
+  res.status(200).send({ response: message });
 });
 
 app.listen(port, () => {
